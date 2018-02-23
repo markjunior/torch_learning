7db41b488a05714e99243c8308054089a3e31b00
markjunior <markasjunior@gmail.com>
2018-02-23 09:59:11 -0800

    Create basic_classification.py

---
+++ scr/basic_classification.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+
+# make fake data
+n_data = torch.ones(100, 2)
+x0 = torch.normal(2*n_data, 1)      # class0 x data (tensor), shape=(100, 2)
+y0 = torch.zeros(100)               # class0 y data (tensor), shape=(100, 1)
+x1 = torch.normal(-2*n_data, 1)     # class1 x data (tensor), shape=(100, 2)
+y1 = torch.ones(100)                # class1 y data (tensor), shape=(100, 1)
+x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit
+y = torch.cat((y0, y1), ).type(torch.LongTensor)    # shape (200,) LongTensor = 64-bit integer
+
+x, y = Variable(x), Variable(y)
+
+class Net(torch.nn.Module):
+    def __init__(self, n_features, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_features, n_hidden)
+        self.out = torch.nn.Linear(n_hidden, n_output)
+        
+        
+    def forward(self, x):
+        x = F.relu(self.hidden(x))
+        x = self.out(x)
+        return x
+        
+        
+net= Net(2, 10, 2)
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+
+loss_func = torch.nn.CrossEntropyLoss()
+
+
+plt.ion()   # something about plotting
+
+for t in range(1000):
+    out = net(x)
+    
+    loss = loss_func(out, y)
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    if t % 2 == 0:
+        # plot and show learning process
+        plt.cla()
+        prediction = torch.max(F.softmax(out), 1)[1]
+        pred_y = prediction.data.numpy().squeeze()
+        target_y = y.data.numpy()
+        plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
+        accuracy = sum(pred_y == target_y)/1000.
+        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+        
+plt.ioff()
+plt.show()
